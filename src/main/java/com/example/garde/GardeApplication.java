39b1f3cb6e7d7b57f27c285e4cfefd4bc5e3260d
Jahir Zakaria <jahir.zakaria.work@gmail.com>
2023-11-19 23:16:07 +0100

    first commit

---
+++ src/main/java/com/example/garde/GardeApplication.java
@@ -0,0 +1,13 @@
+package com.example.garde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GardeApplication.class, args);
+    }
+
+}
